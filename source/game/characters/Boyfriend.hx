10710c213ffba4743283d61a8485979565a32c43
Yoshman29 <81191621+YoshiCrafter29@users.noreply.github.com>
2023-07-22 02:55:43 +0200

    Update Boyfriend.hx

--- source/game/characters/Boyfriend.hx
+++ source/game/characters/Boyfriend.hx
@@ -4,6 +4,7 @@ class Boyfriend extends SpriteCharacter {
     public function new(x:Float, y:Float, flip:Bool) {
         super(x, y, flip);
         flipX = !flipX; // bf is flipped
+        antialiasing = true;
         this.loadFrames('game/characters/bf');
     }
 }
